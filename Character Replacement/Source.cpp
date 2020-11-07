24c47043c1520e046da12d0c7edbbf3bf920aa87
Chanon <pcg.mega@gmail.com>
2020-11-07 21:14:26 +0700

    for A

--- Character Replacement/Source.cpp
+++ Character Replacement/Source.cpp
@@ -1,41 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void replacement(char*, char, char);
-int stringLength(char*);
+void replacement(char*, char, char, char*);
 
 int main()
 {
-	char text[1000], find, replace;
+	int count = 0;
+	char text[1000], find, replace, request[100];
 	cin.getline(text, 1000);
 	cin >> find >> replace;
-	replacement(&text[0], find, replace);
+	cin >> request;
+	replacement(&text[0], find, replace, &request[0]);
+
 	cout << text;
 
 	return 0;
 }
 
-void replacement(char* s, char f, char r)
-{
-	int i = 0;
-	while (1)
-	{ 
-		if (s[i] != '\0')
-		{
-			if (s[i] == f)
-			{
-				s[i] = r;
-				break;
-			}
-			i++;
-			if (i == stringLength(&s[0])) break;
-		}
-	}
-}
-
-int stringLength(char* s)
+void replacement(char* str, char fnd, char rp, char* rq)
 {
 	int n = 0;
-	for (int i = 0;s[i] != '\0';i++) n++;
-	return n;
+	for (int i = 0;str[i] != '\0';i++)
+	{
+		if (str[i] == fnd)
+		{
+			if (rq[n] == 'Y' || rq[n] == 'y')
+			{
+				rq[n] = rp;
+				str[i] = rq[n];
+			}
+			n++;
+		}
+	}
 }
